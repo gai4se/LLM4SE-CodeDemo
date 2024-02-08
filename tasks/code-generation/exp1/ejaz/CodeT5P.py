ea1a6806d415ab1c7bcc244cb39f82eabd0514f0
eshaik <ejazahmed7051@gmail.com>
2024-02-08 12:48:14 -0500

    Evaluation scripts

--- tasks/code-generation/exp1/ejaz/CodeT5P.py
+++ tasks/code-generation/exp1/ejaz/CodeT5P.py
@@ -11,11 +11,55 @@ model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint,
                                               trust_remote_code=True).to(device)
 print(model.get_memory_footprint())
 
-# python_function ="Write python code for linear regression" # text-to-code
-python_function ="write python code to print first 10 primes"
-encoding = tokenizer (python_function, return_tensors="pt").to(device)
+java_function ="public boolean isEven(int num){ //Complete"
+encoding = tokenizer (java_function, return_tensors="pt").to(device)
 encoding['decoder_input_ids'] = encoding['input_ids'].clone()
-outputs = model.generate(**encoding, max_length=175)
+outputs = model.generate(**encoding, max_length=300)
 generated_code = tokenizer.decode (outputs[0], skip_special_tokens=True)
 
 print(generated_code)
+#Output - public boolean isEven(int num){ //Complete return num%2==0; }
+
+java_function ="public int sumNumbers(int a, int b){ //Complete"
+encoding = tokenizer (java_function, return_tensors="pt").to(device)
+encoding['decoder_input_ids'] = encoding['input_ids'].clone()
+outputs = model.generate(**encoding, max_length=300)
+generated_code = tokenizer.decode (outputs[0], skip_special_tokens=True)
+
+print(generated_code)
+#Output - public int sumNumbers(int a, int b){ //Complete    return a + b; //Complete}
+
+java_function ="public int findMaximum(int a, int b){ //Complete"
+encoding = tokenizer (java_function, return_tensors="pt").to(device)
+encoding['decoder_input_ids'] = encoding['input_ids'].clone()
+outputs = model.generate(**encoding, max_length=300)
+generated_code = tokenizer.decode (outputs[0], skip_special_tokens=True)
+
+print(generated_code)
+
+''' Output - public int findMaximum(int a, int b){ //Complete    if(a>b){ //If a is greater than b
+        return a; //If a is less than b
+    }
+    return b; //If a is equal to b
+}
+'''
+
+java_function ="public int factorial(int num){ //Complete"
+encoding = tokenizer (java_function, return_tensors="pt").to(device)
+encoding['decoder_input_ids'] = encoding['input_ids'].clone()
+outputs = model.generate(**encoding, max_length=300)
+generated_code = tokenizer.decode (outputs[0], skip_special_tokens=True)
+
+print(generated_code)
+
+#Output - public int factorial(int num){ //Complete return num! * (num! + 1) // 2 }
+
+java_function ="public String reverseString(String input){ //Complete"
+encoding = tokenizer (java_function, return_tensors="pt").to(device)
+encoding['decoder_input_ids'] = encoding['input_ids'].clone()
+outputs = model.generate(**encoding, max_length=300)
+generated_code = tokenizer.decode (outputs[0], skip_special_tokens=True)
+
+print(generated_code)
+
+#Output - public String reverseString(String input){ //Complete return input[::-1]; //Complete}
